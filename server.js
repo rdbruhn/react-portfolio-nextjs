a108b03908b994d6336857830d2872a3c7ea4c6e
Rob Bruhn <rob.bruhn@raise.com>
2018-12-27 22:40:22 -0500

    Add compression to server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const next = require("next");
+const compression = require("compression");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -9,7 +10,7 @@ app
   .prepare()
   .then(() => {
     const server = express();
-
+    server.use(compression());
     server.get("/post/:id", (req, res) => {
       const actualPage = "/post";
       const queryParams = { title: req.params.id };
