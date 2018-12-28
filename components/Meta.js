e8d03c6dd6c9296e3584c6e21e84af00085d0e98
Rob Bruhn <rob.bruhn@raise.com>
2018-12-28 02:06:26 -0500

    Add canonical link and title to head

--- components/Meta.js
+++ components/Meta.js
@@ -2,8 +2,10 @@ import Head from "next/head";
 export default () => (
   <div>
     <Head>
+      <title>moduloMoments</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
+      <link rel="canonical" href="http://www.modulomoments.com" />
       <meta charSet="utf-8" />
     </Head>
     <style jsx global>{`
